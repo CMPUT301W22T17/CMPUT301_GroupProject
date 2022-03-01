372988926b33b1c98caf0c5ef0d4036800765a0c
Jan Chow <tobelhead@gmail.com>
2022-03-01 10:12:14 -0700

    Added basic incomplete functions for the Map class

--- SuperQR/app/src/main/java/com/example/superqr/Map.java
+++ SuperQR/app/src/main/java/com/example/superqr/Map.java
@@ -1,13 +1,40 @@
 package com.example.superqr;
 
+import java.util.ArrayList;
+
+/**
+ * Map is a class that displays the locations of nearby QR codes and the player's location
+ */
+
 public class Map {
     private ArrayList<QRCode> QRCodeLocations;
     private Player player;
-    private MapView mapView;
     private GoogleMap QRMap;
 
     public Map(ArrayList<QRCode> QRCodeLocations, Player player) {
         this.QRCodeLocations = QRCodeLocations;
         this.player = player;
     }
+
+    /**
+     * Displays the map for the player to see
+     */
+    public void displayMap() {
+        // WIP
+    }
+
+    /**
+     * Places the locations of nearby QR codes on the map
+     */
+    public void placeQRLocations() {
+        // WIP
+    }
+
+    /**
+     * Places the location of the current player
+     */
+    public void placePlayerLocation() {
+        // WIP
+    }
+
 }
