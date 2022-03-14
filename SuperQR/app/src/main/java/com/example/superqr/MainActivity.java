9b142e323eec4ab08a1949fea9a1fb2fad1c239a
wenhan6 <wenhan6@ualberta.ca>
2022-03-13 19:30:24 -0600

    minor change in MainActivity

--- SuperQR/app/src/main/java/com/example/superqr/MainActivity.java
+++ SuperQR/app/src/main/java/com/example/superqr/MainActivity.java
@@ -37,6 +37,9 @@ import com.google.firebase.firestore.DocumentReference;
 import com.google.firebase.firestore.DocumentSnapshot;
 import com.google.firebase.firestore.FieldValue;
 import com.google.firebase.firestore.FirebaseFirestore;
+
+import java.util.HashMap;
+import java.util.Map;
 public class MainActivity extends AppCompatActivity implements EditInfoFragment.OnFragmentInteractionListener, ScanFragment.ScanFragmentListener {
 
     private ActivityMainBinding binding;
