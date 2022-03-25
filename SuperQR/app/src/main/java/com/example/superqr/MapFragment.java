7e3907629de72c746dbe1dc1e93e9b12f665c3c2
Jan Chow <tobelhead@gmail.com>
2022-03-25 14:44:58 -0600

    Map functionality that displays nearby qr codes in a user searched location (and also changed icon colours)

--- SuperQR/app/src/main/java/com/example/superqr/MapFragment.java
+++ SuperQR/app/src/main/java/com/example/superqr/MapFragment.java
@@ -1,21 +1,30 @@
 package com.example.superqr;
 
+import static android.content.ContentValues.TAG;
+
 import android.content.Context;
 import android.graphics.Bitmap;
 import android.graphics.drawable.BitmapDrawable;
 import android.graphics.drawable.Drawable;
+import android.icu.text.DecimalFormat;
+import android.location.Address;
+import android.location.Geocoder;
 import android.location.Location;
+import android.os.Build;
 import android.os.Bundle;
 
+import androidx.annotation.RequiresApi;
 import androidx.core.content.res.ResourcesCompat;
 import androidx.fragment.app.Fragment;
 
 import android.os.StrictMode;
 import android.preference.PreferenceManager;
+import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.Button;
+import android.widget.EditText;
 import android.widget.ImageButton;
 import android.widget.LinearLayout;
 import android.widget.Toast;
@@ -33,8 +42,10 @@ import org.osmdroid.views.MapController;
 import org.osmdroid.views.MapView;
 import org.osmdroid.views.overlay.Marker;
 
+import java.io.IOException;
 import java.util.ArrayList;
 import java.util.List;
+import java.util.Locale;
 import java.util.Objects;
 
 /**
@@ -51,16 +62,19 @@ public class MapFragment extends Fragment implements View.OnClickListener {
     // At: https://osmdroid.github.io/osmdroid/How-to-use-the-osmdroid-library.html
     // Reference: https://osmdroid.github.io/osmdroid/javadocs/osmdroid-android/debug/index.html?org/osmdroid/views/MapView.html
 
-    MapView map;
-    MapController controller;
-    Marker playerMarker;
-    Drawable playerPin;
-    Drawable QRPin;
-    GeoPoint playerPoint;
-    LocationStore singleCodeLocation = null;
-    ImageButton zoomInButton;
-    ImageButton zoomOutButton;
-    double radius = 0.15;
+    private MapView map;
+    private MapController controller;
+    private Marker playerMarker;
+    private Marker locationMarker;
+    private Drawable playerPin;
+    private Drawable QRPin;
+    private Drawable locationPin;
+    private GeoPoint playerPoint;
+    private LocationStore singleCodeLocation = null;
+    private ImageButton zoomInButton;
+    private ImageButton zoomOutButton;
+    private ImageButton searchLocationButton;
+    private EditText locationSearchText;
 
     public MapFragment() {
         // Required empty public constructor
@@ -109,6 +123,11 @@ public class MapFragment extends Fragment implements View.OnClickListener {
         zoomOutButton = view.findViewById(R.id.zoom_out_button);
         zoomOutButton.setOnClickListener(this);
 
+        searchLocationButton = view.findViewById(R.id.search_location_button);
+        searchLocationButton.setOnClickListener(this);
+
+        locationSearchText = (EditText) view.findViewById(R.id.location_search_text);
+
         Bundle codeLocationBundle = getArguments();
         if (codeLocationBundle != null) {
             singleCodeLocation = codeLocationBundle.getParcelable("code_location");
@@ -132,6 +151,7 @@ public class MapFragment extends Fragment implements View.OnClickListener {
             playerPoint = new GeoPoint(playerLocation);
             setToLocation(playerPoint);
             addLocationMarkers();
+            addQRLocationMarkers(playerLocation, 0.04);
         }
 
         else {
@@ -177,6 +197,19 @@ public class MapFragment extends Fragment implements View.OnClickListener {
             case R.id.zoom_out_button:
                 controller.zoomOut();
                 break;
+            case R.id.search_location_button:
+                String searchedLocation = locationSearchText.getText().toString();
+                if (searchedLocation != "") {
+                    try {
+                        searchLocation(searchedLocation);
+                    } catch (IOException e) {
+                        e.printStackTrace();
+                    }
+                }
+                else {
+                    Toast.makeText(getActivity(), "No location entered.", Toast.LENGTH_SHORT).show();
+                }
+                break;
         }
     }
 
@@ -187,12 +220,16 @@ public class MapFragment extends Fragment implements View.OnClickListener {
         //https://stackoverflow.com/questions/60301641/customized-icon-in-osmdroid-marker-android
         // Player Map Marker Icon: <a href="https://www.flaticon.com/free-icons/location" title="location icons">Location icons created by IconMarketPK - Flaticon</a>
         // QR Map Marker Icon: <a href="https://www.flaticon.com/free-icons/location" title="location icons">Location icons created by IconMarketPK - Flaticon</a>
+        // Searched Location Map Marker Icon: <a href="https://www.flaticon.com/free-icons/marker" title="marker icons">Marker icons created by IconMarketPK - Flaticon</a>
 
         Drawable initialPlayerPin = ResourcesCompat.getDrawable(getResources(), R.drawable.player_marker, null);
         playerPin = new BitmapDrawable(getResources(), Bitmap.createScaledBitmap((((BitmapDrawable) initialPlayerPin).getBitmap()), (int) (33.0f * getResources().getDisplayMetrics().density), (int) (36.0f * getResources().getDisplayMetrics().density), true));
 
         Drawable initialQRPin = ResourcesCompat.getDrawable(getResources(), R.drawable.qr_marker, null);
         QRPin = new BitmapDrawable(getResources(), Bitmap.createScaledBitmap((((BitmapDrawable) initialQRPin).getBitmap()), (int) (33.0f * getResources().getDisplayMetrics().density), (int) (36.0f * getResources().getDisplayMetrics().density), true));
+
+        Drawable initialLocationPin = ResourcesCompat.getDrawable(getResources(), R.drawable.location_marker, null);
+        locationPin = new BitmapDrawable(getResources(), Bitmap.createScaledBitmap((((BitmapDrawable) initialLocationPin).getBitmap()), (int) (33.0f * getResources().getDisplayMetrics().density), (int) (36.0f * getResources().getDisplayMetrics().density), true));
 
         playerMarker = new Marker(map);
         playerMarker.setIcon(playerPin);
@@ -221,28 +258,26 @@ public class MapFragment extends Fragment implements View.OnClickListener {
         playerMarker.setTitle(Double.toString(player.getPlayerLocation().getLatitude()) + ", " + Double.toString(player.getPlayerLocation().getLongitude()));
         playerMarker.setAnchor(Marker.ANCHOR_CENTER, Marker.ANCHOR_BOTTOM);
         map.getOverlays().add(playerMarker);
-
-        addQRLocationMarkers();
     }
 
     /**
      * Put markers onto the map of the QR codes that are nearby the player
      */
-    private void addQRLocationMarkers() {
+    private void addQRLocationMarkers(Location centerLocation, double radius) {
         FirebaseFirestore db = FirebaseFirestore.getInstance();
 
         db.collection("codes").get()
                 .addOnSuccessListener(new OnSuccessListener<QuerySnapshot>() {
+                    @RequiresApi(api = Build.VERSION_CODES.N)
                     @Override
                     public void onSuccess(QuerySnapshot queryDocumentSnapshots) {
                         if (!queryDocumentSnapshots.isEmpty()) {
                             List<DocumentSnapshot> list = queryDocumentSnapshots.getDocuments();
                             for (DocumentSnapshot d : list) {
                                 QRCode code = d.toObject(QRCode.class);
-                                double latDifference = (double) (Math.abs(code.getLocation().getLatitude() - player.getPlayerLocation().getLatitude()));
-                                double longDifference = (double) (Math.abs(code.getLocation().getLongitude() - player.getPlayerLocation().getLongitude()));
+                                double latDifference = (double) (Math.abs(code.getLocation().getLatitude() - centerLocation.getLatitude()));
+                                double longDifference = (double) (Math.abs(code.getLocation().getLongitude() - centerLocation.getLongitude()));
                                 if (latDifference < radius && longDifference < radius) {
-
                                     Location location = new Location("map_location");
                                     location.setLatitude(code.getLocation().getLatitude());
                                     location.setLongitude(code.getLocation().getLongitude());
@@ -252,7 +287,15 @@ public class MapFragment extends Fragment implements View.OnClickListener {
                                     Marker QRMarker = new Marker(map);
                                     QRMarker.setIcon(QRPin);
                                     QRMarker.setPosition(QRPoint);
-                                    QRMarker.setTitle(Double.toString(code.getLocation().getLatitude()) + ", " + Double.toString(code.getLocation().getLongitude()));
+
+                                    double markerLatDifference = getLocationDifference(centerLocation.getLatitude(), code.getLocation().getLatitude());
+                                    double markerLongDifference = getLocationDifference(centerLocation.getLongitude(), code.getLocation().getLongitude());
+
+                                    // https://stackoverflow.com/questions/2538787/how-to-print-a-float-with-2-decimal-places-in-java
+                                    DecimalFormat decimalRounder = new DecimalFormat();
+                                    decimalRounder.setMaximumFractionDigits(4);
+
+                                    QRMarker.setTitle("(" + Double.toString(Double.parseDouble(decimalRounder.format(markerLatDifference))) + ", " + Double.toString(Double.parseDouble(decimalRounder.format(markerLongDifference))) + ") km away.");
                                     QRMarker.setAnchor(Marker.ANCHOR_CENTER, Marker.ANCHOR_BOTTOM);
                                     map.getOverlays().add(QRMarker);
                                 }
@@ -260,6 +303,46 @@ public class MapFragment extends Fragment implements View.OnClickListener {
                         }
                     }
                 });
+    }
+
+    private void searchLocation(String searchedLocation) throws IOException {
+        // https://stackoverflow.com/questions/69148288/how-to-search-location-name-on-osmdroid-to-get-latitude-longitude
+        try {
+            Geocoder geocoder = new Geocoder(getActivity(), Locale.getDefault());
+            List<Address> geoResults = geocoder.getFromLocationName(searchedLocation, 1);
+            if (!geoResults.isEmpty()) {
+
+                map.getOverlays().remove(locationMarker);
+                Address address = geoResults.get(0);
+                Location location = new Location("map_location");
+                location.setLatitude(address.getLatitude());
+                location.setLongitude(address.getLongitude());
+                GeoPoint foundPoint = new GeoPoint(address.getLatitude(), address.getLongitude());
+
+                locationMarker = new Marker(map);
+                locationMarker.setIcon(locationPin);
+                setToLocation(foundPoint);
+                locationMarker.setPosition(foundPoint);
+
+                locationMarker.setTitle(Double.toString(address.getLatitude()) + ", " + Double.toString(address.getLongitude()));
+                locationMarker.setAnchor(Marker.ANCHOR_CENTER, Marker.ANCHOR_BOTTOM);
+                map.getOverlays().add(locationMarker);
+
+                locationSearchText.getText().clear();
+                addQRLocationMarkers(location, 0.05);
+
+            }
+            else {
+                Toast.makeText(getActivity(), "Cannot find location.", Toast.LENGTH_SHORT).show();
+            }
+        }
+        catch (IOException error) {
+            Log.d(TAG, error.getMessage());
+        }
+    }
+
+    private double getLocationDifference(double centerPoint, double otherPoint) {
+        return centerPoint - otherPoint;
     }
 
 }
